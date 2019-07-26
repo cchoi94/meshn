f6844cffb416497aa8fbdda21f13d15e91903704
Cody Choi <cchoi_2294@hotmail.com>
2019-07-26 19:02:28 -0400

    [env] added server env for netlify

---
+++ scripts/create-env.js
@@ -0,0 +1,13 @@
+const fs = require('fs')
+
+fs.writeFileSync('./.env', `API_KEY=${process.env.API_KEY}\n`)
+
+fs.writeFileSync('./.env', `AUTH_DOMAIN=${process.env.AUTH_DOMAIN}\n`)
+
+fs.writeFileSync('./.env', `DATABASE_UR=${process.env.DATABASE_UR}\n`)
+
+fs.writeFileSync('./.env', `PROJECT_ID=${process.env.PROJECT_ID}\n`)
+
+fs.writeFileSync('./.env', `MESSAGING_SENDER_ID=${process.env.MESSAGING_SENDER_ID}\n`)
+
+fs.writeFileSync('./.env', `SITE_KEY=${process.env.SITE_KEY}\n`)
