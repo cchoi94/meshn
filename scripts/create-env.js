71543f0bb0dfa171575115003b1b1297b0f420eb
Cody Choi <cchoi_2294@hotmail.com>
2019-07-26 20:50:41 -0400

    [Netlify] altered env settings

--- scripts/create-env.js
+++ scripts/create-env.js
@@ -4,7 +4,7 @@ fs.writeFileSync('./.env', `API_KEY=${process.env.API_KEY}\n`)
 
 fs.writeFileSync('./.env', `AUTH_DOMAIN=${process.env.AUTH_DOMAIN}\n`)
 
-fs.writeFileSync('./.env', `DATABASE_UR=${process.env.DATABASE_UR}\n`)
+fs.writeFileSync('./.env', `DATABASE_URL=${process.env.DATABASE_UR}\n`)
 
 fs.writeFileSync('./.env', `PROJECT_ID=${process.env.PROJECT_ID}\n`)
 
